9eae2fbf25c71d992c488c8d37e3890ad6c63700
Sergey Marenin <mss9836@gmail.com>
2022-01-10 10:31:49 +0700

    ci: configured logout

--- blog-api/src/main/java/com/mareninss/blogapi/config/SecurityConfig.java
+++ blog-api/src/main/java/com/mareninss/blogapi/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -40,8 +41,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .formLogin().disable()
         .httpBasic().disable()
         .logout()
-        .logoutSuccessUrl("/**")
-        .clearAuthentication(true);
+        .logoutUrl("/**/logout")
+        .logoutSuccessUrl("/")
+        .invalidateHttpSession(true)
+        .and()
+        .exceptionHandling()
+        .accessDeniedPage("/403");
   }
 
   @Bean
